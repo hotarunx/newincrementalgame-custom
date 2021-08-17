6e9575b2347f900fa33f8f40429b64422c6bb25b
hotarunx <29631883+hotarunx@users.noreply.github.com>
2021-08-17 23:55:50 +0900

    add header and fix bug

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 "use strict";
-confirm = function () {
-    return true;
-};
-function clickAnyButton() {
-    const rButton = document.querySelector("#levelreset > button");
-    if (rButton != null) {
-        rButton.click();
-    }
-    const gButtons = document.querySelectorAll(".gbutton, .abutton");
-    let aimButton = gButtons[0];
-    gButtons.forEach(element => {
-        if (element.className.indexOf("unavailable") > 0) {
-            aimButton = element;
+// ==UserScript==
+// @name            新しい放置ゲーム自作自動購入機
+// @namespace       https://github.com/hotarunx
+// @homepage        https://github.com/hotarunx/nig-auto-buy-bot
+// @version         0.0.1
+// @author          hotarunx
+// @match           https://dem08656775.github.io/newincrementalgame/
+// @grant           none
+// ==/UserScript==
+(() => {
+    confirm = () => true;
+    function clickAnyButton() {
+        const rButton = document.querySelector("#levelreset > button");
+        if (rButton != null) {
+            rButton.click();
         }
-    });
-    if (aimButton.className.indexOf("unavailable") > 0) {
-        aimButton.click();
+        const gButtons = document.querySelectorAll(".gbutton, .abutton");
+        let aimButton = gButtons[0];
+        gButtons.forEach(element => {
+            if (element.className.indexOf("unavailable") < 0) {
+                aimButton = element;
+            }
+        });
+        if (aimButton.className.indexOf("unavailable") < 0) {
+            aimButton.click();
+        }
     }
-}
-const coinamount = document.querySelector("#coinamount");
-if (coinamount != null) {
-    coinamount.addEventListener("DOMNodeInserted", clickAnyButton);
-}
-clickAnyButton();
+    const coinamount = document.querySelector("#coinamount");
+    if (coinamount != null) {
+        coinamount.addEventListener("DOMNodeInserted", clickAnyButton);
+    }
+    clickAnyButton();
+})();
--- script.ts
+++ script.ts
@@ -1,29 +1,39 @@
-confirm = function () {
-    return true;
-};
+// ==UserScript==
+// @name            新しい放置ゲーム自作自動購入機
+// @namespace       https://github.com/hotarunx
+// @homepage        https://github.com/hotarunx/nig-auto-buy-bot
+// @version         0.0.1
+// @author          hotarunx
+// @match           https://dem08656775.github.io/newincrementalgame/
+// @grant           none
+// ==/UserScript==
 
-function clickAnyButton() {
-    const rButton: HTMLElement | null = document.querySelector("#levelreset > button");
-    if (rButton != null) {
-        rButton.click();
-    }
+(() => {
+    confirm = () => true;
 
-    const gButtons: NodeListOf<HTMLElement> = document.querySelectorAll(".gbutton, .abutton");
-
-    let aimButton: HTMLElement = gButtons[0];
-    gButtons.forEach(element => {
-        if (element.className.indexOf("unavailable") > 0) {
-            aimButton = element;
+    function clickAnyButton() {
+        const rButton: HTMLElement | null = document.querySelector("#levelreset > button");
+        if (rButton != null) {
+            rButton.click();
         }
-    });
 
-    if (aimButton.className.indexOf("unavailable") > 0) {
-        aimButton.click();
+        const gButtons: NodeListOf<HTMLElement> = document.querySelectorAll(".gbutton, .abutton");
+
+        let aimButton: HTMLElement = gButtons[0];
+        gButtons.forEach(element => {
+            if (element.className.indexOf("unavailable") < 0) {
+                aimButton = element;
+            }
+        });
+
+        if (aimButton.className.indexOf("unavailable") < 0) {
+            aimButton.click();
+        }
     }
-}
 
-const coinamount = document.querySelector("#coinamount");
-if (coinamount != null) {
-    coinamount.addEventListener("DOMNodeInserted", clickAnyButton);
-}
-clickAnyButton();
+    const coinamount = document.querySelector("#coinamount");
+    if (coinamount != null) {
+        coinamount.addEventListener("DOMNodeInserted", clickAnyButton);
+    }
+    clickAnyButton();
+})();
