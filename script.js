1f70f815458ec0c84e920c50e4897fdb21d55d73
hotarunx <29631883+hotarunx@users.noreply.github.com>
2021-08-22 12:12:12 +0900

    update comment

--- script.js
+++ script.js
@@ -9,20 +9,24 @@
 // @grant           none
 // ==/UserScript==
 (() => {
+    // すべての発生器購入・時間加速器購入・段位リセットボタンを押す
     function clickAllButton() {
         // confirm, alertを無視する
         confirm = () => true;
         alert = () => true;
+        // 段位リセットボタンを押す
         const rButton = document.querySelector("#levelreset > button");
         if (rButton != null) {
             rButton.click();
         }
+        // 発生器・時間加速器購入ボタンを押す
         const buttonList = document.querySelectorAll(".gbutton, .abutton");
         const buttonArray = Array.from(buttonList);
         buttonArray.reverse().forEach(element => {
             element.click();
         });
     }
+    // clickAllButton関数をVueインスタンスのupdateに含める
     const container = window.document.getElementById("app");
     const app = container.__vue_app__;
     const ctx = app._instance.ctx;
