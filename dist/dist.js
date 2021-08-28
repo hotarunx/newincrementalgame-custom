069f97625c217531b36b2a54c8617343380ef2e6
hotarunx <29631883+hotarunx@users.noreply.github.com>
2021-08-29 03:07:46 +0900

    fix eslint alert

--- dist/dist.js
+++ dist/dist.js
@@ -19,21 +19,21 @@
         confirm = () => true;
         alert = () => true;
         // 段位リセットボタンを押す
-        const rButton = document.querySelector("#levelreset > button");
+        const rButton = document.querySelector('#levelreset > button');
         if (rButton != null) {
             rButton.click();
         }
         // 発生器・時間加速器購入ボタンを押す
-        const buttonList = document.querySelectorAll(".gbutton, .abutton");
+        const buttonList = document.querySelectorAll('.gbutton, .abutton');
         const buttonArray = Array.from(buttonList);
-        buttonArray.reverse().forEach(element => {
+        buttonArray.reverse().forEach((element) => {
             element.click();
         });
         confirm = confirmOrg;
         alert = alertOrg;
     }
     // clickAllButton関数をVueインスタンスのupdateに含める
-    const container = window.document.getElementById("app");
+    const container = window.document.getElementById('app');
     const app = container.__vue_app__;
     const ctx = app._instance.ctx;
     const _update = ctx.update.bind(ctx);
--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,16 @@
         alert = () => true;
 
         // 段位リセットボタンを押す
-        const rButton: HTMLElement | null = document.querySelector("#levelreset > button");
+        const rButton: HTMLElement | null = document.querySelector('#levelreset > button');
         if (rButton != null) {
             rButton.click();
         }
 
         // 発生器・時間加速器購入ボタンを押す
-        const buttonList: NodeListOf<HTMLElement> = document.querySelectorAll(".gbutton, .abutton");
+        const buttonList: NodeListOf<HTMLElement> = document.querySelectorAll('.gbutton, .abutton');
         const buttonArray = Array.from(buttonList);
 
-        buttonArray.reverse().forEach(element => {
+        buttonArray.reverse().forEach((element) => {
             element.click();
         });
 
@@ -26,7 +26,7 @@
     }
 
     // clickAllButton関数をVueインスタンスのupdateに含める
-    const container = window.document.getElementById("app");
+    const container = window.document.getElementById('app');
     const app = container.__vue_app__;
     const ctx = app._instance.ctx;
     const _update = ctx.update.bind(ctx);
